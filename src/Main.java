748f3abe5a2bda5618dbe632d5ff81854c9d7746
CabbaGG2 <jcamaradepablos@danielcastelao.org>
2025-09-12 13:53:14 +0200

    creacion de projecto y primeros metodos

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.io.File;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String rutaRealDirectorio = "/Users/nombre_usuario";
+        String rutaRealFichero = "/Users/home/dam/primerScript.py";
+
+        String resultado1 = esDirectorio(rutaRealFichero);
+        System.out.println(resultado1);
+
+        String resultado2 = esFichero(rutaRealFichero);
+        System.out.println(resultado2);
+    }
+
+    /**
+     * metodo que regresa un String dependiendo si la ruta que se le pasa es real o no
+     * @param ruta
+     * @return String
+     */
+    public static String esDirectorio(String ruta) {
+        File archivo = new File(ruta);
+
+        boolean respuesta = archivo.isDirectory();
+
+        if (respuesta){
+            return "Es un directorio.";
+        } else {
+            return  "No es un directorio";
+        }
+
+    }
+
+    public static String esFichero(String ruta) {
+        File archivo = new File(ruta);
+
+        boolean respuesta = archivo.isFile();
+
+        if (respuesta){
+            return "Es un archivo.";
+        } else {
+            return  "No es un archivo";
+        }
+    }
+
+
+}
